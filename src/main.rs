73018ec2578d97c238bfc5f7140b59bbc4f16fe9
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-07-21 20:30:57 +0200

    Sampling more pixels

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use glam::f32::Vec3;
+use glam::f32::{ Vec3, Vec2 };
 
 struct Ray {
     origin: Vec3,
@@ -12,6 +12,39 @@ impl Ray {
 
     fn at(&self, t: f32) -> Vec3 {
         self.origin + self.dir * t
+    }
+}
+
+struct Camera {
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    fn new() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let viewport_height = 2.0;
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.0;
+
+        let origin = Vec3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+        Self {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical
+        }
+    }
+
+    fn get_ray(&self, uv: Vec2) -> Ray {
+        let dir = self.lower_left_corner + uv.x * self.horizontal + uv.y * self.vertical - self.origin;
+        Ray::new(self.origin, dir)
     }
 }
 
@@ -30,7 +63,6 @@ fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> f32 {
 }
 
 fn ray_color(ray: &Ray) -> Vec3 {
-
     let t = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, ray);
     if t > 0.0 {
         let normal = (ray.at(t) - Vec3::new(0.0, 0.0, -1.0)).normalize();
@@ -53,21 +85,43 @@ fn ray_color(ray: &Ray) -> Vec3 {
     (1.0 - t) * color1 + t * color2
 }
 
+fn write_pixel_to_image(image: &mut bmp::Image,
+                        image_width: usize, image_height: usize,
+                        x: usize, y: usize,
+                        color: Vec3,
+                        samples_per_pixel: usize)
+{
+
+    let r = color.x;
+    let g = color.y;
+    let b = color.z;
+
+    let scale = 1.0 / samples_per_pixel as f32;
+
+    let r = (r * scale).sqrt();
+    let g = (g * scale).sqrt();
+    let b = (b * scale).sqrt();
+
+    let r = r.clamp(0.0, 0.999);
+    let g = g.clamp(0.0, 0.999);
+    let b = b.clamp(0.0, 0.999);
+
+    let r = (r * 255.0) as u8;
+    let g = (g * 255.0) as u8;
+    let b = (b * 255.0) as u8;
+
+    image.set_pixel(x as u32, (image_height - y - 1) as u32,
+                    bmp::Pixel::new(r, g, b));
+}
+
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 1920;
+    let image_width = 400;
     let image_height = (image_width as f32 / aspect_ratio) as usize;
     println!("Width: {} Height: {}", image_width, image_height);
 
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-    println!("Viewport Width: {} Height: {}", viewport_width, viewport_height);
-
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let samples_per_pixel = 100;
+    let camera = Camera::new();
 
     let mut image = bmp::Image::new(image_width as u32, image_height as u32);
 
@@ -75,25 +129,22 @@ fn main() {
         let per = ((y as f32 / image_height as f32) * 100.0) as u32;
         print!("\rWorking: {}%", per);
         for x in 0..image_width {
-            let u = x as f32 / image_width as f32;
-            let v = y as f32 / image_height as f32;
+            let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
 
-            let dir = lower_left_corner + horizontal * u + vertical * v  - origin;
-            let ray = Ray::new(origin, dir);
-            let color = ray_color(&ray);
+            for sample in 0..samples_per_pixel {
+                let u = (x as f32 + rand::random::<f32>()) / image_width as f32;
+                let v = (y as f32 + rand::random::<f32>()) / image_height as f32;
+                let uv = Vec2::new(u, v);
 
-            /*
-            let r = x as f32 / image_width as f32;
-            let g = y as f32 / image_height as f32;
-            let b = 0.25;
-            */
+                let ray = camera.get_ray(uv);
+                let color = ray_color(&ray);
+                pixel_color += color;
+            }
 
-            let r = (color.x * 255.0) as u8;
-            let g = (color.y * 255.0) as u8;
-            let b = (color.z * 255.0) as u8;
-
-            image.set_pixel(x as u32, (image_height - y - 1) as u32,
-                            bmp::Pixel::new(r, g, b));
+            write_pixel_to_image(&mut image, image_width, image_height,
+                                 x, y,
+                                 pixel_color,
+                                 samples_per_pixel);
         }
     }
     println!();
